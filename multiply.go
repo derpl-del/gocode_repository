8e8c154f1b50f364a7ea01ac130ffa78cfade32b
unknown <gilsptr@gmail.com>
2020-06-03 15:25:46 +0700

    First commit

--- multiply.go
+++ multiply.go
@@ -1,9 +1,6 @@
 package gomodmultiplies
 
-func Multiply(items ...int64) int64 {
-	res := int64(1)
-	for _, item := range items {
-		res *= item
-	}
-	return res
+//Multiply funct
+func Multiply(items1 int, items2 int) int {
+	return items1 * items2
 }
